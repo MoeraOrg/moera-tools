2629d8e7f4771209a1b95f9c986e4ee8d9a9c823
Shmuel Melamud <smelamud@redhat.com>
2023-09-05 23:29:40 +0300

    Require requests>=2.31.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='moera-tools',
     version='0.1.0',
     install_requires=[
-        'requests',
+        'requests>=2.31.0',
         'camel-converter',
         'python-dateutil',
     ],
