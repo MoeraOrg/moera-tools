da59050c437be0799833b01a014ea2311ae8ff43
Shmuel Melamud <smelamud@redhat.com>
2024-06-19 00:37:34 +0300

    Upgrade moeralib.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='moera-tools',
     version='0.3.1',
     install_requires=[
-        'moeralib~=0.14.5',
+        'moeralib~=0.15.3',
         'python-dateutil',
         'PyYAML',
         'first',
