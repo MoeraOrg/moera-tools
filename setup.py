2bfb8d484c7ccc005e1ab0a4ddf4e26c08aa25d4
Shmuel Melamud <smelamud@redhat.com>
2024-06-26 03:00:20 +0300

    End of release cycle.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='moera-tools',
-    version='0.3.1',
+    version='0.4.0',
     install_requires=[
-        'moeralib~=0.15.3',
+        'moeralib~=0.15.4',
         'python-dateutil',
         'PyYAML',
         'first',
