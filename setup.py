b2eeac6deb0086998784c873da474ee556277f6a
Shmuel Melamud <smelamud@redhat.com>
2024-08-27 00:17:54 +0300

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='moera-tools',
-    version='0.4.0',
+    version='0.5.0',
     install_requires=[
         'moeralib~=0.15.5',
         'python-dateutil',
